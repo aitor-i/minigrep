c80966623b07e4a7f4d1af90c8c2379e63747527
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-06 16:33:10 +0200

    REFACTOR - Split logic to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
+use minigrep::run;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,41 +18,5 @@ fn main() {
     if let Err(e) = run(config) { 
         println!("Application error: {e}");
         process::exit(1);
-    }
-    fn run(config: Config)-> Result<(), Box<dyn Error>>{ 
-        
-        let contents = fs::read_to_string(&config.file_path)?;
-
-        println!("With text: \n{}", &contents);
-
-        return Ok(());
-    }
-
-    
-
-    struct Config { 
-        query: String,
-        file_path: String
-    }
-
-    impl Config { 
-        fn new (args: &[String]) -> Config { 
-            let query = args[1].clone();
-            let file_path = args[2].clone();
-
-            Config {query, file_path}
-
-        }
-        fn build (args: &[String]) ->Result<Config, &'static str> { 
-            if args.len() < 3 { 
-                    return Err("Not enought arguments!");
-            }
-            let query = args[1].clone();
-            let file_path = args[2].clone();
-
-            Ok(Config {query, file_path})
-
-        }
-        
     }
 }
