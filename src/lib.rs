19cafcb1993a3ca43dd4cdb807ebbe14e0d7bb37
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-06 16:55:08 +0200

    TEST - One result

--- src/lib.rs
+++ src/lib.rs
@@ -26,5 +26,29 @@ impl Config {
         Ok(Config {query, file_path})
 
     }
+}
 
+pub fn search<'a>(query:&str, contents: &'a str) -> Vec<&'a str> { 
+    vec![]
+}
+
+# [cfg(test)]
+mod tests{ 
+
+    use super::*;
+
+    #[test]
+    fn one_result(){ 
+        let query = "duct";
+        let contents = "\
+            I show less than I have to stay tasteful
+            Still catching hate cause they’re hateful
+            Too much food out the gate it’s a plateful
+            Now your girls full of tate and she’s grateful";
+
+        assert_eq!(
+            vec!["Still catching hate cause they’re hateful"],
+            search(query, contents)
+        );
+    }
 }
