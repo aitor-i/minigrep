a3bb3495a6db2c8dc1380fe3693d6fd388dadea5
Aitor Ibarra <ibarraaitor@protonmail.com>
2023-10-10 18:31:42 +0200

    UPDATE - Error messages for standar output on errors

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err|{ 
-        println!("Error on parsing arguments: {err}");
+        eprintln!("Error on parsing arguments: {err}");
         process::exit(1); // kills the process
     });
 
 
     if let Err(e) = run(config) { 
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
